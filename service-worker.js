96288eb40aa9ca95374856c7b950876bb76f3f89
Lloydpraise <your@email.com>
2025-11-20 16:13:46 +0300

    manifest json added

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = "vvstudios-cache-v1";
+const ASSETS_TO_CACHE = [
+  "/", 
+  "/index.html",
+  "/manifest.json",
+
+  // icons
+  "/icons/icon-48.png",
+  "/icons/icon-72.png",
+  "/icons/icon-96.png",
+  "/icons/icon-144.png",
+  "/icons/icon-192.png",
+  "/icons/icon-256.png",
+  "/icons/icon-384.png",
+  "/icons/icon-512.png"
+];
+
+// Install SW
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate SW
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch handler
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
